d5e2a41ee33990c0e1c0c09838f21ab30c273598
yoonjae <backendINFJ@github.com>
2024-05-16 16:35:47 +0900

    일정관리(ScheduleManagement) CRUD 전체 리팩토링 (Service는 아직 구현x)

--- src/main/java/com/sparta/personalproject/repository/ScheduleManagementRepository.java
+++ src/main/java/com/sparta/personalproject/repository/ScheduleManagementRepository.java
@@ -1,9 +1,14 @@
 package com.sparta.personalproject.repository;
 
+import com.sparta.personalproject.dto.ScheduleManagementResponseDto;
 import com.sparta.personalproject.entity.ScheduleManagement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // 컴포넌트 스캔방식으로 자동 의존관계 설정 ,  @Repository 안해도 되는 이유 SimpleJpaRepository 관련 설명
+import java.util.List;
+
+@Repository // 컴포넌트 스캔방식으로 자동 의존관계 설정
 public interface ScheduleManagementRepository extends JpaRepository<ScheduleManagement,Long> {
-} //
+
+    List<ScheduleManagementResponseDto> findAllByOrderByModifiedAtDesc(); // 내림차순 정렬
+}
